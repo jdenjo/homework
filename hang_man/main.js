b8f0305c9e23b903a3b2b300c876e160b438fb66
Dean Enjo <jdenjo@gmail.com>
2019-03-11 17:28:35 -0700

    upload final solution

--- hang_man/main.js
+++ hang_man/main.js
@@ -1,28 +1,24 @@
 let mystery_word = "stranger"
 let miss_count = 0;
-let max_miss = 8;
+let max_miss = 6;
 let hit_count = 0;
 let guessed_letters = new Set();
 
 let win_audio = new Audio('./sounds/win.mp3');
 let lose_audio = new Audio('./sounds/lose.mp3');
 
-
 mystery_word = mystery_word.toUpperCase();
 let word_array = mystery_word.split("");
 
 $(document).ready(() => {
 
-  // add the bars
   for (let index = 0; index < mystery_word.length; index++) {
     $(".bar_area").append(`<div id = "${index}" class="bar"> ___ </div>`);
   }
 
-  //  sees if the letter is included and if it does changes 1 or many bars 
   $('li').on('mousedown', (event) => {
-    if (miss_count < 6) {
+    if (miss_count < max_miss) {
       $(event.target).attr('style', 'background-color: red; color:white');
-
       if (!guessed_letters.has(event.currentTarget.innerHTML)) {
         for (let index = 0; index < mystery_word.length; index++) {
           if (word_array[index] === event.currentTarget.innerHTML) {
@@ -31,37 +27,28 @@ $(document).ready(() => {
           }
         }
       }
-
-
     }
   });
 
-  //track hits and misses and end game if needed
   $('li').on('mouseup', (event) => {
-
     if (!guessed_letters.has(event.currentTarget.innerHTML)) {
-
       if (!mystery_word.includes(event.currentTarget.innerHTML)) {
-        if (miss_count < 6)
+        if (miss_count < max_miss)
           miss_count += 1;
         $("#hangman-image").attr("src", `./images/${miss_count + 1}.jpg`);
-        if (miss_count >= 6) {
-          lose_audio.play();
-          alert("Better luck next time!")
-        }
+      }
+      if (miss_count >= 6) {
+        lose_audio.play();
+        setTimeout(function(){alert("Better luck next time!")}, 200);
       } else {
         if (hit_count >= mystery_word.length) {
           win_audio.play();
-          alert("Congratulations! You win!");
-
+          setTimeout(function(){alert("Congratulations! You win!")}, 200);
         }
       }
     }
-
+    //this adds guessed letters so the user doesnt push twice
     guessed_letters.add(event.currentTarget.innerHTML);
-    
   });
-
-
-
 });
+
